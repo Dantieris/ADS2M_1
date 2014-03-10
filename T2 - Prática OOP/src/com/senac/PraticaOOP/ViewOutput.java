0f9ecf17925642423599c70562cc749a8d2b1341
Dantieris Rabelini <dantieris@gmail.com>
2014-03-10 16:59:20 -0300

    Projeto T2 - Prática OOP: Classe ViewOutput adição do método imprimeContato() e documentação do mesmo.

--- T2 - Prática OOP/src/com/senac/PraticaOOP/ViewOutput.java
+++ T2 - Prática OOP/src/com/senac/PraticaOOP/ViewOutput.java
@@ -8,4 +8,13 @@ public class ViewOutput
 	{
 		System.out.println(mensagem);
 	}
+	
+	//imprime o contato na tela em uma linha.
+	//parâmetro String nome nome do contato, String endereco é o endereço do contato, 
+	//e String telefones[] é um vetor que armazena três telefones do contato.
+	public void imprimeContato(String nome, String endereco, String telefones[])
+	{
+		System.out.printf("Nome: %s - Endereço: %s - Celular: %s - Residencial %s - Comercial: %s" , nome, endereco,
+																			telefones[0], telefones [1], telefones[2]);
+	}
 }
