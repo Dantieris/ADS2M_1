b6abc1168e7293f7712de85d8f831e20e9710f39
Dantieris Rabelini <dantieris@gmail.com>
2014-03-10 16:08:44 -0300

    Projeto T2 - Prática OOP: Classe Controler criação do método recebeTelefones() e documentação do mesmo.

--- T2 - Prática OOP/src/com/senac/PraticaOOP/Controler.java
+++ T2 - Prática OOP/src/com/senac/PraticaOOP/Controler.java
@@ -12,4 +12,21 @@ public class Controler
 		
 	}
 
+	//recebe três números de telefone: celular, residencial e comercial.
+	//retorno String[] telefone vetor com três strings de telefone.
+	public String[] recebeTelefones()
+	{
+		String telefone[] = new String [3];
+		
+		impressor.imprimeLinha( "Digite o telefone celular: " );
+		telefone [0] =  leitor.recebeLinha();
+		
+		impressor.imprimeLinha( "Digite o telefone residencial: " );
+		telefone [1] = leitor.recebeLinha();
+		
+		impressor.imprimeLinha( "Digite o telefone comercial: " );
+		telefone [2] = leitor.recebeLinha();
+		
+		return telefone;
+	}
 }
