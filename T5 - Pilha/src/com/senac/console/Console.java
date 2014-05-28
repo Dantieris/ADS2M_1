ec1cd1d0ee83de53963964b803c3ced499b0546e
Dantieris Castilhos Rabelini <dantieris@gmail.com>
2014-05-28 10:36:05 -0300

    Documentacao da classe Console.

--- T5 - Pilha/src/com/senac/console/Console.java
+++ T5 - Pilha/src/com/senac/console/Console.java
@@ -4,14 +4,29 @@ import java.util.Scanner;
 
 public class Console {
 	
+	/**
+	 * Recebe uma String digitada pelo usuário, e a retorna.
+	 * 
+	 * @return A próxima String digitada.
+	 */
 	public String nextString() {
 		return scanner().next().trim().toLowerCase();
 	}
 	
-	public void printLine( String ms ) {
-		System.out.println( ms );
+	/**
+	 * Imprime uma mensagem na tela.
+	 * 
+	 * @param ms A mensagem a ser impressa.
+	 */
+	public void printLine( String mensagem ) {
+		System.out.println( mensagem );
 	}
 
+	/**
+	 * Retorna um objeto Scanner.
+	 * 
+	 * @return O objeto Scanner.
+	 */
 	private Scanner scanner() {
 		return new Scanner(System.in);
 	}
