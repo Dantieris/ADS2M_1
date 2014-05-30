85e88176346ef8ef02e9a970113b0d4698085014
Dantieris Castilhos Rabelini <dantieris@gmail.com>
2014-05-30 06:04:45 -0300

    Add menu com opcoes para testar as TO.

--- T5 - Pilha/src/com/senac/applicacao/AppTestes.java
+++ T5 - Pilha/src/com/senac/applicacao/AppTestes.java
@@ -1,6 +1,8 @@
 package com.senac.applicacao;
 
+import com.senac.controles.CalculadorExpressoes;
 import com.senac.controles.ConversorBases;
+import com.senac.controles.VerificadorPalindromo;
 import com.senac.estruturas.exceptions.ContainerEmptyException;
 import com.senac.estruturas.exceptions.ContainerFullException;
 
@@ -13,14 +15,51 @@ import com.senac.estruturas.exceptions.ContainerFullException;
  */
 public class AppTestes {
 	
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		while ( true ) {
+			menu();
+		}
+	}
+	
+	private static void conversorBasesTest() {
 		ConversorBases cb = new ConversorBases();
 		
-		while(true) {
-			System.out.println( "Digite o número decimal:" );
-			System.out.println( cb.converterDecimal( new java.util.Scanner(System.in).nextInt() ) );
-		}
+		System.out.println( "Digite o número decimal:" );
+		System.out.println( cb.converterDecimal( new java.util.Scanner(System.in).nextInt() ) );
+	}
+	
+	private static void verificadorPalimdromoTest() {
+		VerificadorPalindromo vp = new VerificadorPalindromo();
 		
+		System.out.println( "Digite a palavra:" );
+		System.out.println( vp.verificaPalimdromo( new java.util.Scanner(System.in).next() ) );
+	}
+	
+	private static void calculadorExpressoesTest() {
+		CalculadorExpressoes ce = new CalculadorExpressoes();
+		
+		System.out.println( "Digite a expressao infixa:" );
+		System.out.println( ce.calcularExpressao( new java.util.Scanner(System.in).next() ) );
+	}
+	
+	private static void menu() {
+		char op = 0;
+		
+		while( op != 's' ) {
+			System.out.println( "\n=== Menu de Testes ==="
+					+ "\n1 - Teste Conversor de Bases."
+					+ "\n2 - Teste Verificaodr de Palindromos."
+					+ "\n3 - Teste Calculador de Expressoes Matematicas Infixas."
+					+ "\ns - Sair.");
+			
+			op = new java.util.Scanner(System.in).next().charAt(0);
+			
+			switch( op ) {
+			case '1' : conversorBasesTest(); break;
+			case '2' : verificadorPalimdromoTest(); break;
+			case '3' : calculadorExpressoesTest(); break;
+			default  : System.out.println( "Opção inválida" );
+			}
+		}
 	}
 }
