9066e21d262dd69cdf58e65211d276ea867b8dac
Dantieris Castilhos Rabelini <dantieris@gmail.com>
2014-05-31 17:33:52 -0300

    Add put, get, getLength, getDados, setLength todos documentados.

--- Estruturas de Dados/src/com/senac/estruturas/vetor/Vetor.java
+++ Estruturas de Dados/src/com/senac/estruturas/vetor/Vetor.java
@@ -6,7 +6,7 @@ package com.senac.estruturas.vetor;
  *
  */
 public class Vetor<T> {
-	protected Object vetor[];
+	protected Object[] dados;
 	
 	/**
 	 * Inicializa um objeto Vetor<T> recém-criado com o tamanho definido pelo parametro.
@@ -14,7 +14,7 @@ public class Vetor<T> {
 	 * @param tamanho O tamanho do vetor.
 	 */
 	public Vetor( int tamanho ){
-		this.vetor = new Object [tamanho];
+		this.dados = new Object [tamanho];
 	}
 	
 	/**
@@ -22,7 +22,65 @@ public class Vetor<T> {
 	 * 
 	 */
 	public Vetor() {
-		this.vetor = new Object [1];
+		this.dados = new Object [1];
+	}
+	
+	/**
+	 * Modifica um elemento do vetor.
+	 * 
+	 * @param indice Posição do elemento no vetor.
+	 * @param valor Valor a ser modificado.
+	 */
+	public void put(int indice, T valor) {
+		this.dados[indice] = valor;
+	}
+	
+	/**
+	 * Acessa um item do vetor.
+	 * 
+	 * @param indice Posição do item.
+	 * @return O valor na posição do indice.
+	 */
+	@SuppressWarnings("unchecked")
+	public T get(int indice) {
+		return (T) this.dados[indice];
+	}
+	
+	/**
+	 * Acessa o tamanho do vetor.
+	 * 
+	 * @return Um inteiro contetndo o tamanho do vetor.
+	 */
+	public int getLength() {
+		return dados.length;
+	}
+	
+	/**
+	 * Acessa o vetor de dados.
+	 
+	 * @return O vetor de dados.
+	 */
+	@SuppressWarnings("unchecked")
+	public T[] getDados() {
+		return (T[]) dados;
+	}
+	
+	/**
+	 * Modifica o tamanho do vetor.
+	 * 
+	 * @param newLength O novo tamanho.
+	 */
+	public void setLength( int newLength ) {
+		if ( dados.length != newLength ) {
+			Object [] newDados = new Object[newLength];
+			
+			int min = dados.length < newLength ?
+					dados.length : newLength;
+			
+			for ( int i = 0 ; i < min ; i++ )
+				newDados [i] = dados [i];
+			dados = newDados;
+		}
 	}
 	
 }
